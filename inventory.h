5508bcdfd8a995ed0c3605b0d63d6d33998361f8
teddy walsh <teddyjwalsh@gmail.com>
2022-04-25 18:33:09 -0500

    Added shop prototype. Added shop hud start to inventory

--- inventory.h
+++ inventory.h
@@ -4,11 +4,14 @@
 #include "stat_interface.h"
 #include "widget_component.h"
 #include "physics_component.h"
+#include "camera.h"
 
 struct CompInventory : public StatInterface, public CompWidget
 {
 
     std::array<EntityRef, 6> items;
+    bool is_shop;
+    bgfx::Camera* camera;
 
     virtual StatPart get_stat(Stat stat) override;
 
@@ -16,6 +19,7 @@ struct CompInventory : public StatInterface, public CompWidget
     {
         int index = 0;
         bool active = true;
+        if (!is_shop)
         {
             auto entity_pos = sibling<CompPosition>()->pos;
 
@@ -24,6 +28,46 @@ struct CompInventory : public StatInterface, public CompWidget
                 int widget_height = 150;
                 int widget_width = 250;
                 ImGui::SetNextWindowPos(ImVec2(CompWidget::window_width - (10 + widget_width), CompWidget::window_height - (10 + widget_height)));
+                //ImGui::SetNextWindowPos(ImVec2(CompWidget::window_width - (10 + widget_width), 10));
+                ImGui::SetNextWindowSize(ImVec2(widget_width, widget_height));
+                ImGui::Begin("Inventory", &active, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize);
+
+                int item_count = 6;
+                int column_count = 3;
+                int row_count = 2;
+                ImGui::Columns(column_count, "inventory_columns");
+                int index = 0;
+                for (auto& item : items)
+                {
+                    index++;
+                    //if (item.is_valid())
+                    {
+                        ImGui::ImageButton(0, ImVec2(widget_height/2 - 30, widget_height/2 - 30));
+                        {
+                            //ImGui::Text((std::string("CD: ") + std::to_string(ab->current_cooldown.value())).c_str());
+                        }
+                    }
+                    if (index % row_count == 0)
+                    {
+                        ImGui::NextColumn();
+                    }
+                }
+                ImGui::Columns();
+                ImGui::End();
+            }
+        }
+        else
+        {
+            auto entity_pos = sibling<CompPosition>()->pos;
+            {
+                bool active = true;
+                int widget_height = 150;
+                int widget_width = 250;
+
+                glm::vec4 screen_space = camera->world_to_screen_space(entity_pos);
+                ImVec2 p = ImVec2(screen_space.x - 40, CompWidget::window_height - screen_space.y - 50);
+                ImGui::SetNextWindowPos(ImVec2(p.x - 4, p.y-2));
+
                 //ImGui::SetNextWindowPos(ImVec2(CompWidget::window_width - (10 + widget_width), 10));
                 ImGui::SetNextWindowSize(ImVec2(widget_width, widget_height));
                 ImGui::Begin("Inventory", &active, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize);
---
+++ shop_proto.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "entity_prototype.h"
+#include "ability.h"
+#include "animation_component.h"
+#include "lifetime_system.h"
+#include "health_component.h"
+#include "has_owner_component.h"
+#include "interactable_component.h"
+#include "shell_level_component.h"
+#include "team_component.h"
+#include "caster_component.h"
+
+struct ShopProto : public ActorProto
+{
+
+    ShopProto(const glm::vec3& in_pos, const std::vector<CompType>& extension_types={}):
+        ActorProto(in_pos, extension_types)
+    {
+        std::vector<CompType> unit_components = {{
+                    uint32_t(type_id<CompPhysics>), 
+                    uint32_t(type_id<CompBounds>),
+                    uint32_t(type_id<CompStaticMesh>),
+                    uint32_t(type_id<CompInventory>),
+                    uint32_t(type_id<CompLifetime>),
+                    uint32_t(type_id<CompInteractable>),
+            }};
+        append_components(unit_components);
+    }
+
+    virtual void init(EntityRef entity) 
+    {
+        auto cube_mesh = std::make_shared<bgfx::Mesh>();
+        cube_mesh->load_obj("cube.obj");
+        entity.cmp<CompPosition>()->scale = glm::vec3(1, 1, 5);
+        entity.cmp<CompStaticMesh>()->mesh.set_mesh(cube_mesh);
+        auto box_mat = std::make_shared<bgfx::Material>();
+
+        std::ifstream t("C:\\Users\\tjwal\\projects\\ECS\\materials\\box_mat\\VertexShader.glsl");
+        std::stringstream buffer;
+        buffer << t.rdbuf();
+        auto vshader = std::make_shared<Shader>(Shader::Type::Vertex, buffer.str(), true);
+        std::ifstream t2("C:\\Users\\tjwal\\projects\\ECS\\materials\\box_mat\\FragmentShader.glsl");
+        std::stringstream buffer2;
+        buffer2 << t2.rdbuf();
+        auto fshader = std::make_shared<Shader>(Shader::Type::Fragment, buffer2.str(), true);
+        box_mat->set_vertex_shader(vshader);
+        box_mat->set_fragment_shader(fshader);
+        box_mat->link();
+        entity.cmp<CompStaticMesh>()->mesh.set_material(box_mat);
+        entity.cmp<CompStaticMesh>()->mesh.set_id(entity.get_id());
+        entity.cmp<CompPosition>()->pos = glm::vec3(100,100,20);
+        entity.cmp<CompInventory>()->visible = true;
+        /*
+        entity.cmp<CompAnimation>()->start_time = 0;
+        entity.cmp<CompAnimation>()->end_time = 50;
+        entity.cmp<CompAnimation>()->start_scale = glm::vec3(1.0);
+        entity.cmp<CompAnimation>()->end_scale = glm::vec3(5.0);
+        */
+    }
+};
