c5b7238fbf68f8116d5887f2f5b20874706d5389
Teddy Walsh <teddyjwalsh@gmail.com>
2022-04-26 13:15:38 -0500

    Improved shop gui, added item name to component

--- shop_system.h
+++ shop_system.h
@@ -79,56 +79,52 @@ class SysShop : public GuiSystem
         }
         auto* camera = &cameras[0].graphics_camera;
         bool active = true;
+        bool shopper_in_range = true;
+        auto entity_pos = shop_inventory->sibling<CompPosition>()->pos;
+        auto* shop_interatee = shop_inventory->sibling<CompInteractable>();
+        if (auto* shopper_pos = shopper_wallet->sibling<CompPosition>())
         {
+            if (glm::length(shopper_pos->pos - entity_pos) > shop_interactee->interact_range)
             {
-                bool shopper_in_range = true;
-                auto entity_pos = shop_inventory->sibling<CompPosition>()->pos;
-                auto* shop_interatee = shop_inventory->sibling<CompInteractable>();
-                if (auto* shopper_pos = shopper_wallet->sibling<CompPosition>())
-                {
-                    if (glm::length(shopper_pos->pos - entity_pos) > shop_interactee->interact_range)
-                    {
-                        shopper_in_range = false;
-                        shop_inventory->visible = false;
-                        return;
-                    }
-                }
-                bool active = true;
-                int widget_height = 150;
-                int widget_width = 250;
-
-                glm::vec4 screen_space = camera->world_to_screen_space(entity_pos);
-                ImVec2 p = ImVec2(screen_space.x - 40, CompWidget::window_height - screen_space.y - 50);
-                ImGui::SetNextWindowPos(ImVec2(p.x - 4, p.y-2));
-
-                //ImGui::SetNextWindowPos(ImVec2(CompWidget::window_width - (10 + widget_width), 10));
-                ImGui::SetNextWindowSize(ImVec2(widget_width, widget_height));
-                ImGui::Begin("ShopInventory", &active, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize);
-
-                int item_count = 6;
-                int column_count = 3;
-                int row_count = 2;
-                ImGui::Columns(column_count, "inventory_columns");
-                int index = 0;
-                for (auto& slot : shop_inventory->slots)
-                {
-                    index++;
-                    if (ImGui::ImageButton(0, ImVec2(widget_height/2 - 30, widget_height/2 - 30)))
-                    {
-                        if (slot.item_entity.is_valid())
-                        {
-                            bool bought = trigger_buy(shopper_wallet, shop_inventory, slot);
-                        }
-                        //ImGui::Text((std::string("CD: ") + std::to_string(ab->current_cooldown.value())).c_str());
-                    }
-                    if (index % row_count == 0)
-                    {
-                        ImGui::NextColumn();
-                    }
-                }
-                ImGui::Columns();
-                ImGui::End();
+                shopper_in_range = false;
+                shop_inventory->visible = false;
+                return;
             }
         }
+        int widget_height = 150;
+        int widget_width = 250;
+
+        glm::vec4 screen_space = camera->world_to_screen_space(entity_pos);
+        ImVec2 p = ImVec2(screen_space.x - 40, CompWidget::window_height - screen_space.y - 50);
+        ImGui::SetNextWindowPos(ImVec2(p.x - 4, p.y-2));
+
+        //ImGui::SetNextWindowPos(ImVec2(CompWidget::window_width - (10 + widget_width), 10));
+        ImGui::SetNextWindowSize(ImVec2(widget_width, widget_height));
+        ImGui::Begin("ShopInventory", &active, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize);
+
+        int item_count = shop_inventory->slots.size();
+        int column_count = 3;
+        int row_count = std::ceil(item_count/column_count);
+        ImGui::Columns(column_count, "inventory_columns");
+        int index = 0;
+        for (auto& slot : shop_inventory->slots)
+        {
+            index++;
+            auto item_name = slot.item_entity.cmp<CompItem>()->name;
+            if (ImGui::ImageButton(0, ImVec2(widget_height/2 - 30, widget_height/2 - 30)))
+            {
+                if (slot.item_entity.is_valid())
+                {
+                    bool bought = trigger_buy(shopper_wallet, shop_inventory, slot);
+                }
+            }
+            ImGui::Text(item_name.c_str());
+            if (index % row_count == 0)
+            {
+                ImGui::NextColumn();
+            }
+        }
+        ImGui::Columns();
+        ImGui::End();
     }
 };
