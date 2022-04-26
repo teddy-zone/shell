45713e11aed6068d18b24cc75ee054db1341aeed
teddy walsh <teddyjwalsh@gmail.com>
2022-04-25 22:59:10 -0500

    Moved inventory and health/ability widgets to systems

--- inventory.h
+++ inventory.h
@@ -15,44 +15,6 @@ struct CompInventory : public StatInterface, public CompWidget
 
     virtual void tick() 
     {
-        int index = 0;
-        bool active = true;
-        {
-            auto entity_pos = sibling<CompPosition>()->pos;
-
-            {
-                bool active = true;
-                int widget_height = 150;
-                int widget_width = 250;
-                ImGui::SetNextWindowPos(ImVec2(CompWidget::window_width - (10 + widget_width), CompWidget::window_height - (10 + widget_height)));
-                //ImGui::SetNextWindowPos(ImVec2(CompWidget::window_width - (10 + widget_width), 10));
-                ImGui::SetNextWindowSize(ImVec2(widget_width, widget_height));
-                ImGui::Begin("Inventory", &active, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize);
-
-                int item_count = 6;
-                int column_count = 3;
-                int row_count = 2;
-                ImGui::Columns(column_count, "inventory_columns");
-                int index = 0;
-                for (auto& item : items)
-                {
-                    index++;
-                    //if (item.is_valid())
-                    {
-                        ImGui::ImageButton(0, ImVec2(widget_height/2 - 30, widget_height/2 - 30));
-                        {
-                            //ImGui::Text((std::string("CD: ") + std::to_string(ab->current_cooldown.value())).c_str());
-                        }
-                    }
-                    if (index % row_count == 0)
-                    {
-                        ImGui::NextColumn();
-                    }
-                }
-                ImGui::Columns();
-                ImGui::End();
-            }
-        }
     };
 
 };
--- shop_inventory.h
+++ shop_inventory.h
@@ -6,56 +6,11 @@
 #include "physics_component.h"
 #include "camera.h"
 
-struct CompShopInventory : public CompWidget
+struct CompShopInventory : public Component
 {
 
     std::array<EntityRef, 6> items;
     bgfx::Camera* camera;
-
-
-    virtual void tick() 
-    {
-        int index = 0;
-        bool active = true;
-        {
-            auto entity_pos = sibling<CompPosition>()->pos;
-            {
-                bool active = true;
-                int widget_height = 150;
-                int widget_width = 250;
-
-                glm::vec4 screen_space = camera->world_to_screen_space(entity_pos);
-                ImVec2 p = ImVec2(screen_space.x - 40, CompWidget::window_height - screen_space.y - 50);
-                ImGui::SetNextWindowPos(ImVec2(p.x - 4, p.y-2));
-
-                //ImGui::SetNextWindowPos(ImVec2(CompWidget::window_width - (10 + widget_width), 10));
-                ImGui::SetNextWindowSize(ImVec2(widget_width, widget_height));
-                ImGui::Begin("ShopInventory", &active, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize);
-
-                int item_count = 6;
-                int column_count = 3;
-                int row_count = 2;
-                ImGui::Columns(column_count, "inventory_columns");
-                int index = 0;
-                for (auto& item : items)
-                {
-                    index++;
-                    //if (item.is_valid())
-                    {
-                        ImGui::ImageButton(0, ImVec2(widget_height/2 - 30, widget_height/2 - 30));
-                        {
-                            //ImGui::Text((std::string("CD: ") + std::to_string(ab->current_cooldown.value())).c_str());
-                        }
-                    }
-                    if (index % row_count == 0)
-                    {
-                        ImGui::NextColumn();
-                    }
-                }
-                ImGui::Columns();
-                ImGui::End();
-            }
-        }
-    };
+    bool visible = false;
 
 };
